1a8b17db7debe6117d7a4f5ca90caa1e18f722c6
mbp1225 <matheus698@hotmail.com>
2018-04-13 06:15:18 -0300

    Initial commit

---
+++ Assets/Scripts/Gameplay/CellController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellController : MonoBehaviour
+{
+    [SerializeField] Vector2Int pos;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+
+    public void Setup (int x, int y)
+    {
+        pos.x = x;
+        pos.y = y;
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Cell " + pos.x + " " + pos.y);
+    }
+}
---
+++ Assets/Scripts/Gameplay/GridController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    [SerializeField] Vector2Int gridSize;
+    [SerializeField] GameObject gridCell;
+
+    [SerializeField] GameObject[,] grid;
+    [SerializeField] GameObject[,] gridContents;
+
+    void Start ()
+    {
+        grid = new GameObject[gridSize.x, gridSize.y];
+        gridContents = new GameObject[gridSize.x, gridSize.y];
+        CreateGrid();
+        transform.position += new Vector3(- (float)gridSize.x / 2, - (float)gridSize.y / 2);
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void CreateGrid()
+    {
+        for (int i = 0; i < gridSize.x; i++)
+        {
+            for (int j = 0; j < gridSize.y; j++)
+            {
+                grid[i, j] = Instantiate(gridCell, new Vector2(i + .5f, j + .5f), Quaternion.identity, transform) as GameObject;
+                grid[i, j].GetComponent<CellController>().Setup(i, j);
+            }
+        }
+    }
+}
